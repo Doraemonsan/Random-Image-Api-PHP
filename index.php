3a08b03279dd8f599b588674304e750ff6f31fa1
Doraemonsan <XiaoBaiSama@outlook.com>
2022-09-24 23:56:10 +0800

    modified:   index.php

--- index.php
+++ index.php
@@ -1,36 +1,32 @@
-<?php /*PHP判断是否是移动端*/ 
+<?php /*PHP判断是否是移动端*/
 redirect();
 
-function redirect() { 
+function redirect() {
 
-if ( empty($_SERVER['HTTP_USER_AGENT']) ) 
-{ 
-    $img=file('image-list-mobile');
-    $url=array_rand($img);
-    header("Location:".$img[$url]);
-    header('Access-Control-Allow-Origin:*');
+if ( empty($_SERVER['HTTP_USER_AGENT']) )
+{
+    $img=glob('img/*mpic/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
+    header("Content-Type: image/webp");
+    echo(file_get_contents($img[array_rand($img)]));
 }
-elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false || 
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false ) 
-{ 
-    $img=file('image-list-mobile');
-    $url=array_rand($img);
-    header("Location:".$img[$url]);
-    header('Access-Control-Allow-Origin:*'); 
-    
-} 
-else 
-{ 
-    $img=file('image-list-desktop');
-    $url=array_rand($img);
-    header("Location:".$img[$url]);
-    header('Access-Control-Allow-Origin:*');
-} 
+elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false )
+{
+    $img=glob('img/*mpic/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
+    header("Content-Type:image/webp");
+    echo(file_get_contents($img[array_rand($img)]));
+}
+else
+{
+    $img=glob('img/*ppic/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
+    header("Content-Type:image/webp");
+    echo(file_get_contents($img[array_rand($img)]));
+}
 
-} 
+}
 ?>
